427f29b856bb54735bbebd588001787168a38d5c
Syed Hamza Qadri <qadrihamza871@gmail.com>
2024-03-02 19:47:38 +0500

    all files updated

---
+++ Menu/app.js
@@ -0,0 +1,15 @@
+var water_bottles = +prompt("Quantity of Water Bottles")
+var chapati = +prompt("Quantity of Chapati")
+var cold_drinks = +prompt("Quantity of Cold Drinks")
+var biryani = +prompt("Quantity of Biryani (plate)")
+var pizza = +prompt("Quantity of Pizza")
+
+
+document.write("<h1>Your Order</h1>")
+document.write("<p> You Ordered " + water_bottles + " Water Bottles</p>")
+document.write("<p> You Ordered " + chapati + " Chapati</p>")
+document.write("<p> You Ordered " + cold_drinks + " Cold Drinks</p>")
+document.write("<p> You Ordered " + biryani + " Biryani</p>")
+document.write("<p> You Ordered " + pizza + " Pizza</p>")
+
+document.write("Your Percentage is " + percentage + "%")
